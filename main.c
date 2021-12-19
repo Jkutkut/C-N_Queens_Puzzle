7f8c61a332f48a20a86b2e78c05fc66776f7b130
jkutkut <jorgeregonzalez@gmail.com>
2021-12-20 00:52:46 +0100

    style defined

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+
+#define QUEEN "♛"
+
+#define N 8
+
+/*
++---+-------+
+|   |
+| ♛ |
+|   |
++---+-------+
+*/
+
+void	print_board(void)
+{
+	//write(1, QUEEN, 4);
+	printf("+------+\n");
+	printf("|      |\n");
+	printf("|  ♛   |\n");
+	printf("|      |\n");
+	printf("+------+\n");
+	printf("|······|\n");
+	printf("|··♛ ··|\n");
+	printf("|······|\n");
+	printf("+------+\n");
+
+}
+
+int	main(void)
+{
+	print_board();
+	printf("\n");
+}
