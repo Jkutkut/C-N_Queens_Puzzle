cae314e79b8cfeddf56c35d6f40cd2ed7355fb76
jkutkut <jorgeregonzalez@gmail.com>
2021-12-21 10:50:28 +0100

    general output functions now using common .h file

---
+++ src/output/output.h
@@ -0,0 +1,12 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#ifndef _UNISTD_H
+#include <unistd.h>
+#endif
+
+int		ft_strlen(char *str);
+void	ft_putstr(char *str);
+void	ft_putstrlen(char *str, int l)
+
+#endif
