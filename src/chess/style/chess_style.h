f3118f20748b2cfa609b6454e0009323511a6c2f
jkutkut <jorgeregonzalez@gmail.com>
2021-12-21 10:42:47 +0100

    chess structure now on custom file

---
+++ src/chess/style/chess_style.h
@@ -0,0 +1,10 @@
+typedef struct s_chess_style
+{
+	char *corner;
+	char *v_border;
+	char *h_border;
+	char *white_queen;
+	char *black_queen;
+	char *white_fill;
+	char *black_fill;
+} t_chess_style;
