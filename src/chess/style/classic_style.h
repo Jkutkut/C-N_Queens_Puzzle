ba0d885fc93a8344ac6d41a3bcc5769a64bbb6e7
jkutkut <jorgeregonzalez@gmail.com>
2021-12-21 10:43:25 +0100

    deleted

--- src/chess/style/classic_style.h
+++
@@ -1,3 +0,0 @@
-#define STYLE ((t_chess_style) { .corner = "┼", .v_border = "│", .h_border = "──────", .white_queen = "  ♛   ", .black_queen = "··♛ ··", .white_fill = "      ", .black_fill = "······" })
-
-
