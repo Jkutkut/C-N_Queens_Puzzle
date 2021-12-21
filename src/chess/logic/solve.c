c3b45e406218526e0f58c2b2e683ad8c7830dda6
jkutkut <jorgeregonzalez@gmail.com>
2021-12-21 10:34:25 +0100

    solve now on this file

---
+++ src/chess/logic/solve.c
@@ -0,0 +1,28 @@
+void	solve(char board[N], int *solutions)
+{
+	int	i;
+	int	h;
+
+	i = 0;
+	while (i < N)
+	{
+		if (board[i] == EMPTY)
+		{
+			h = 0;
+			while (h < N)
+			{
+				if (possible(board, i, h + 48))
+				{
+					board[i] = h + 48;
+					solve(board, solutions);
+					board[i] = EMPTY;
+				}
+				h++;
+			}
+			return ;
+		}
+		i++;
+	}
+	print(board);
+	*solutions = *solutions + 1;
+}
