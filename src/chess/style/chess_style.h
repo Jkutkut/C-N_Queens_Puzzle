79c07b6d2f058adeb30018aa3633557ab8d49642
jkutkut <jorgeregonzalez@gmail.com>
2021-12-21 10:44:46 +0100

    .h protection added

--- src/chess/style/chess_style.h
+++ src/chess/style/chess_style.h
@@ -1,3 +1,6 @@
+#ifndef CHESS_STYLE_H
+#define CHESS_STYLE_H
+
 typedef struct s_chess_style
 {
 	char *corner;
@@ -8,3 +11,5 @@ typedef struct s_chess_style
 	char *white_fill;
 	char *black_fill;
 } t_chess_style;
+
+#endif
