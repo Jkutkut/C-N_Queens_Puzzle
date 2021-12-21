f5d00c26259773433fda8f6bc360a564ab16ccda
jkutkut <jorgeregonzalez@gmail.com>
2021-12-21 10:33:42 +0100

    possible queen logic now on this file

---
+++ src/chess/logic/possible_queen.c
@@ -0,0 +1,15 @@
+int	possible_queen(char board[N], int index, char h)
+{
+	int	i;
+
+	i = 0;
+	while (i < index)
+	{
+		if (board[i] == h)
+			return (0);
+		if (board[i] + (index - i) == h || board[i] - (index - i) == h)
+			return (0);
+		i++;
+	}
+	return (1);
+}
