62a1f249462ca9bae9d9b8a45f8ca176bd19413a
= <=>
2021-12-24 16:04:42 +0100

    REPRESENTATION WORKS

--- src/chess/logic/ft_solve.c
+++ src/chess/logic/ft_solve.c
@@ -13,9 +13,9 @@ void	ft_solve(char *board, int N, int *solutions, t_chess_style style)
 			h = 0;
 			while (h < N)
 			{
-				if (ft_possible_queen(board, N, i, h + 48))
+				if (ft_possible_queen(board, N, i, h))
 				{
-					board[i] = h + 48;
+					board[i] = h;
 					ft_solve(board, N, solutions, style);
 					board[i] = EMPTY;
 				}
