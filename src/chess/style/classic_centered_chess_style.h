7d74efbad7368b4ec3869f35a7e6009280542c58
= <=>
2021-12-25 19:48:14 +0100

    new style added

---
+++ src/chess/style/classic_centered_chess_style.h
@@ -0,0 +1,3 @@
+#define STYLE ((t_chess_style) { .corner = "┼", .v_border = "│", .h_border = "───────", .white_queen = "   ♛   ", .black_queen = "···♛···", .white_fill = "       ", .black_fill = "·······" })
+
+
